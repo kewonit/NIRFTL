dbf5c36f5d701d941e2cfc7292f4c6919ad55bc8
KewKartik <108450560+KewKartik@users.noreply.github.com>
2023-06-10 19:16:02 +0530

    added shadcn/ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-import "~/styles/globals.css";
+import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
