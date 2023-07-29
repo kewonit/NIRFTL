751870abf6740d2dd67fa828c4e11f1174b77735
KewKartik <108450560+KewKartik@users.noreply.github.com>
2023-07-30 00:42:35 +0530

    update

--- components/datatable/rtibtech.tsx
+++ components/datatable/rtibtech.tsx
@@ -12,9 +12,9 @@ export const columns: ColumnDef<BTechRTI>[] = [
       header: "Name",
       cell: ({ row }) => {
         const amount = String(row.getValue("Institute"))
-        const formatted = amount
+        const Institute = amount
    
-        return  <div className="text-center whitespace-nowrap">{formatted}</div>
+        return  <div>{Institute}</div>
       },
     },
     {
@@ -24,106 +24,10 @@ export const columns: ColumnDef<BTechRTI>[] = [
     ),
     cell: ({ row }) => {
       const amount = String(row.getValue("Years"))
-      const formatted = amount
+      const Year = amount
   
-      return  <div className="text-center whitespace-nowrap pr-4">{formatted}</div>
+      return  <div>{Year}</div>
     },   
-    },
-    {
-      accessorKey: "Branch",
-      header: ({ column }) =>  (
-        <DataTableColumnHeader column={column} title="Branch" />
-    ),
-    cell: ({ row }) => {
-      const amount = String(row.getValue("Branch"))
-      const formatted = amount
-  
-      return  <div className="text-center whitespace-nowrap pr-4">{formatted}</div>
-    },   
-    },
-    {
-      accessorKey: "Registered",
-      header: ({ column }) =>  (
-          <DataTableColumnHeader column={column} title="Registered" />
-      ),
-        cell: ({ row }) => {
-          const amount = parseFloat(row.getValue("Registered"))
-          const formatted = amount
-     
-          return  <div className="text-center pr-4">{formatted}</div>
-        },
-    },
-    {
-      accessorKey: "Placed",
-      header: ({ column }) =>  (
-        <DataTableColumnHeader column={column} title="Placed" />
-    ),
-      cell: ({ row }) => {
-        const amount = parseFloat(row.getValue("Placed"))
-        const formatted = amount
-   
-        return  <div className="text-center pr-4">{formatted}</div>
-      },
-    },
-    {
-      accessorKey: "Median",
-      header: ({ column }) =>  (
-        <DataTableColumnHeader column={column} title="Median" />
-    ),
-      cell: ({ row }) => {
-        const amount = parseFloat(row.getValue("Median"))
-        const formatted = new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "INR",
-        }).format(amount)
-    
-        return  <div className="text-center font-medium">{formatted}</div>
-      },
-    },
-    {
-      accessorKey: "Average",
-      header: ({ column }) =>  (
-          <DataTableColumnHeader column={column} title="Average" />
-    ),
-      cell: ({ row }) => {
-        const amount = parseFloat(row.getValue("Average"))
-        const formatted = new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "INR",
-        }).format(amount)
-    
-        return  <div className="text-center font-medium">{formatted}</div>
-      },
-    },
-    {
-      accessorKey: "Lowest",
-      header: ({ column }) =>  (
-        <DataTableColumnHeader column={column} title="Lowest" />
-    ),
-      cell: ({ row }) => {
-        const amount = parseFloat(row.getValue("Lowest"))
-        const formatted = new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "INR",
-        }).format(amount)
-    
-        return  <div className="text-center font-medium">{formatted}</div>
-      },
-    },
-    {
-      accessorKey: "Highest",
-      header: ({ column }) =>  (
-        <DataTableColumnHeader column={column} title="Highest" />
-    ),
-      cell: ({ row }) => {
-        const amount = parseFloat(row.getValue("Highest"))
-        const formatted = new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "INR",
-        }).format(amount)
-    
-        return  <div className="text-center font-medium">{formatted}</div>
-      },
     },
     {
       accessorKey: "Source",
@@ -132,8 +36,8 @@ export const columns: ColumnDef<BTechRTI>[] = [
     ),
       cell: ({ row }) => {
         const amount = String(row.getValue("Source"))
-        const formatted = amount
-        return <Button variant="outline" asChild><Link href={formatted}>Source</Link></Button>
+        const Source = amount
+        return <Button variant="outline" asChild><Link href={Source}>Source</Link></Button>
       },
     },  
   ]
